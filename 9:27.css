653f15ae32ee9d057f858540706e98bd690fa232
Ctcooper11 <112099053+Ctcooper11@users.noreply.github.com>
2022-09-29 10:16:42 -0400

    Add files via upload

---
+++ 9:27.css
@@ -0,0 +1,148 @@
+* {
+    font-family: 'Courier New', Courier, monospace;
+    text-decoration:none;
+    color: white;
+    text-align: center;
+}
+.container {  display: grid;
+    grid-template-columns: repeat(8, 1fr);
+    grid-template-rows: repeat(32, 1fr);
+    gap: 0px 0px;
+    grid-auto-flow: row;
+    grid-template-areas:
+      "Header Header Header Header Header Header Header Header"
+      "Header Header Header Header Header Header Header Header"
+      "Header Header Header Header Header Header Header Header"
+      "Header Header Header Header Header Header Header Header"
+      "Header Header Header Header Header Header Header Header"
+      "Sidebar Sidebar Sidebar Sidebar Content Content Content Content"
+      "Sidebar Sidebar Sidebar Sidebar Content Content Content Content"
+      "Sidebar Sidebar Sidebar Sidebar Content Content Content Content"
+      "Sidebar Sidebar Sidebar Sidebar Content Content Content Content"
+      "Sidebar Sidebar Sidebar Sidebar Content Content Content Content"
+      "Sidebar Sidebar Sidebar Sidebar Content Content Content Content"
+      "Sidebar Sidebar Sidebar Sidebar Content Content Content Content"
+      "Sidebar Sidebar Sidebar Sidebar Content Content Content Content"
+      "Sidebar Sidebar Sidebar Sidebar Content Content Content Content"
+      "Footer Footer Footer Footer Footer Footer Sidebar2 Sidebar2"
+      "Footer Footer Footer Footer Footer Footer Sidebar2 Sidebar2"
+      "Footer Footer Footer Footer Footer Footer Sidebar2 Sidebar2"
+      "Footer Footer Footer Footer Footer Footer Sidebar2 Sidebar2"
+      "Footer Footer Footer Footer Footer Footer Sidebar2 Sidebar2"
+      "Sidebar3 Sidebar3 Sidebar3 Sidebar4 Sidebar4 Sidebar4 Sidebar5 Sidebar5"
+      "Sidebar3 Sidebar3 Sidebar3 Sidebar4 Sidebar4 Sidebar4 Sidebar5 Sidebar5"
+      "Sidebar3 Sidebar3 Sidebar3 Sidebar4 Sidebar4 Sidebar4 Sidebar5 Sidebar5"
+      "Sidebar3 Sidebar3 Sidebar3 Sidebar4 Sidebar4 Sidebar4 Sidebar5 Sidebar5"
+      "Main Main Main Main Main Main Main Main"
+      "Main Main Main Main Main Main Main Main"
+      "Main Main Main Main Main Main Main Main"
+      "Main Main Main Main Main Main Main Main"
+      "Main Main Main Main Main Main Main Main"
+      "Main Main Main Main Main Main Main Main"
+    "Last Last Last Last Last Last Last Last"
+    "Last Last Last Last Last Last Last Last"
+    "Last Last Last Last Last Last Last Last"
+    "Last Last Last Last Last Last Last Last"
+   ;
+    height: 200vh;
+    overflow: hidden;
+    grid-gap: 20px;
+}
+  .Header { 
+    grid-area: Header; 
+    background-color: red;
+    text-align: center;
+    padding: 20px;
+    float: center;
+    text-align: center;
+    }
+  
+  .Sidebar { 
+    grid-area: Sidebar; 
+    background-color: palevioletred;
+    padding: 20px;
+    width: 92%;
+    float: left;
+    }
+  
+  .Content { 
+    grid-area: Content; 
+    background-color:tan;
+    padding: 20px;
+    width: 95%;
+    float: right;
+    }
+  
+  .Footer { 
+    grid-area: Footer;
+    background-color: blue; 
+    padding: 20px;
+    width: 95%;
+    }
+
+    .Sidebar2 { 
+        grid-area: Sidebar2; 
+        background-color:palevioletred;
+        padding: 20px;
+        float: right;
+        }
+
+    .Sidebar3 { 
+            grid-area: Sidebar3; 
+            background-color:red;
+            padding: 20px;
+            float: left;
+            }
+    .Sidebar4 { 
+             grid-area: Sidebar4; 
+             background-color:tan;
+             padding: 20px;
+             float: center;
+             width: 90%;
+             }
+        
+    .Sidebar5 { 
+                grid-area: Sidebar5; 
+                background-color: blue;
+                padding: 20px;
+                float: right;
+                width: 110%;
+                text-align: center;
+                    }
+    .Main{
+        grid-area: Main; 
+        background-color:palevioletred;
+        padding: 20px;
+        float: center;
+        width: 110%;
+        text-align: center;
+    }
+    .Last{
+        grid-area:Last; 
+        background-color: tan;
+        padding: 20px;
+        float: center;
+        width: 110%;
+        text-align: center;
+        color: tan;
+    }
+
+
+    @media(max-width:1000px) {
+    .container {  display: grid;
+        grid-template-columns: repeat(1, 1fr);
+        grid-template-rows: repeat(12, 1fr);
+        gap: 0px 0px;
+        grid-auto-flow: row;
+        grid-template-areas:
+          "Header"
+          "Sidebar"
+          "Content"
+          "Footer"
+          "Sidebar2";
+        height: 100vh;
+        overflow: hidden;
+        grid-gap: 20px;
+        
+    }
+    }
